5132dfe3b9b2a306d6604dee0575d76b5da9d6b5
NicoCec <nicolas.dufour@etu.univ-nantes.fr>
2013-04-04 14:07:26 +0200

    modification des licences et en-tete

--- src/main/execution.java
+++ src/main/execution.java
@@ -1,5 +1,7 @@
 /**
+ * License The Apache Software License, Version 2.0
  * Copyright (C) 2013-2013 Nicolas - version 1.0-SNAPSHOT - All rights reserved.
+ * Last modification 04-04-2013
  */
 
 package main;
