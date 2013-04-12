33ae2697bdcf58b8c041ae58cb72ab0110277ddd
NicoCec <nicolas.dufour@etu.univ-nantes.fr>
2013-04-12 15:42:06 +0200

    modification du pom

--- src/main/execution.java
+++ src/main/execution.java
@@ -2,6 +2,7 @@
  * License The Apache Software License, Version 2.0
  * Copyright (C) 2013-2013 Nicolas - version 1.0-SNAPSHOT - All rights reserved.
  * Last modification 12-04-2013
+ * identifiant : ${git.commit.id}
  */
 
 package main;
